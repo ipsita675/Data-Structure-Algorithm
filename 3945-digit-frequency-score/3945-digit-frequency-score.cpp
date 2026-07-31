6a21135420cb66126793e24eb50f93222501fe3c
Ipsita Pandey <137184226+ipsita675@users.noreply.github.com>
2026-07-31 18:19:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (36.86%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        unordered_map<int,int>mp;
+        while(n){
+            int dig=n%10;
+            mp[dig]++;
+            n/=10;
+        }
+        int sum=0;
+        for(auto it:mp){
+            sum+=(it.first*it.second);
+        }
+
+        return sum;
+    }
+};
