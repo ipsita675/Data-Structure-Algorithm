f8255c48a6acc869bb2a50f425609c2fbbef0eed
Ipsita Pandey <137184226+ipsita675@users.noreply.github.com>
2026-08-03 01:32:08 +0530

    Time: 0 ms (100%), Space: 50.5 MB (28.52%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_set<int>s(friends.begin(),friends.end());
+        vector<int>ans;
+        for(auto x:order){
+            if(s.count(x))ans.push_back(x);
+        }
+        return ans;
+    }
+};
