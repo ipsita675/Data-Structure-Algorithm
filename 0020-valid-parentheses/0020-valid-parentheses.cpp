0d6b7d9edca81be0746f1a09c88ba820b5e33ab7
Ipsita Pandey <137184226+ipsita675@users.noreply.github.com>
2026-08-03 11:36:50 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto c:s){
+            if(c=='(' || c=='{' || c=='[')st.push(c);
+            else{
+                if(st.empty())return false;
+                if((c==')' && st.top()=='(')||
+                    (c=='}' && st.top()=='{')||
+                    (c==']' && st.top()=='[')){
+                        st.pop();
+                    }
+                else return false;
+            }
+        }
+        return st.size()==0;
+    }
+};
