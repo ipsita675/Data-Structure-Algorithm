5de76f0541a7b06df3ebfa5ab8032a6f74bf9a93
Ipsita Pandey <137184226+ipsita675@users.noreply.github.com>
2026-08-13 14:21:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.28%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ct=0;
+        while(n){
+            ct+=(n&1);
+            n=n>>1;
+        }
+        return ct;
+    }
+};
